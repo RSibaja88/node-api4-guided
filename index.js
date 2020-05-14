17b6c85626785b2e9f5cbd1a37d9d6f156912764
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-05-13 22:41:03 -0400

    push

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const server = require("./api/server.js");
+require("dotenv").config();
+const port = process.env.PORT;
+console.log("port:", port)
 
-const port = 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+// CI/CD: development => testing => staging => production
